6e38f2d9c7a4d29556dbdb0c97b28b7296bb4a33
arjunram <sparjunram94@gmail.com>
2019-04-26 09:43:30 -0500

    Working version

--- src/rover_control.cpp
+++ src/rover_control.cpp
@@ -20,6 +20,11 @@ NOTES:
 #include <geometry_msgs/Vector3.h>
 #include <geometry_msgs/TransformStamped.h>
 #include <geometry_msgs/Twist.h>
+#include <geometry_msgs/Pose.h>
+#include <std_msgs/Header.h>
+#include <tf/transform_datatypes.h>
+#include "quaternion.h"
+#include "kinova_driver/kinova_comm.h"
 #include <iostream>
 #include <unistd.h>
 #include <cstdlib>
@@ -27,20 +32,57 @@ NOTES:
 #include <stdio.h>
 #include <nav_msgs/Odometry.h>
 //#include "orbot_utils.h"
-#include <SerialPort.h>//to install, use sudo apt-get install libserial-dev
+#include <serial/serial.h>//to install, use sudo apt-get install libserial-dev
+#include <actionlib/client/simple_action_client.h>
+#include <kinova_msgs/ArmPoseAction.h>
+#include <kinova_msgs/HomeArm.h>
+#include <kinova_msgs/HomeArmRequest.h>
+#include <mutex>
+#include <csignal>
+using namespace kinova;
 
-
+ros::Subscriber armcmd;
 ros::Subscriber velcmd, viconspeed;
-bool update;
+bool update = false;
 
 const float pi=3.14159265359;
 const float wMax=122*2*pi/60;
 const float vMax=.0762*wMax/1.424;// m/s
 float rates[4], real_vel[3];
+
 geometry_msgs::Twist goalVelocity;
 bool GoalTimeout = false;
 ros::Time prevGoal;
+
+KinovaPose position;
+//Orbot orbot;
+ros::Time armTime;
+ros::Duration armDelay(0.1);
+kinova_msgs::ArmPoseGoal goal;
+
+
 void getRotationRates(float* rates,float* goal_vel, float* real_vel, float x_cm=0, float y_cm=0, float length=.04, float width=.037, float radius=.0076);
+
+
+
+
+void armCallback(geometry_msgs::Pose pose)
+{
+	double r,p,y;
+	tf::Quaternion quat;
+	std_msgs::Header header;
+	header.frame_id = "m1n6s300_link_base";
+	
+	goal.pose.header = header;
+	goal.pose.pose.position.x = pose.position.x;
+	goal.pose.pose.position.y = pose.position.y;
+	goal.pose.pose.position.z = pose.position.z;
+	goal.pose.pose.orientation.x = pose.orientation.x;
+	goal.pose.pose.orientation.y = pose.orientation.y;
+	goal.pose.pose.orientation.z = pose.orientation.z;
+	goal.pose.pose.orientation.w = pose.orientation.w;
+	update = true;
+}
 
 
 void viconCallback(nav_msgs::Odometry roverspeed)
@@ -49,6 +91,7 @@ void viconCallback(nav_msgs::Odometry roverspeed)
 	real_vel[0] = roverspeed.twist.twist.linear.x;
 	real_vel[1] = roverspeed.twist.twist.linear.y;
 	real_vel[2] = roverspeed.twist.twist.angular.z;
+	ROS_INFO("Vicon");
 
 }
 
@@ -65,7 +108,7 @@ void velocityCallback(geometry_msgs::Twist twist)
 			rates[i]=120;
 		if(rates[i]<-120)
 			rates[i]=-120;
-		rates[i]=round(rates[i]/122*1000);
+		rates[i]=round(rates[i]/122*700);
 		if(rates[i]<100 && rates[i]>-100)
 			rates[i] = 0;
 	}
@@ -76,21 +119,33 @@ void velocityCallback(geometry_msgs::Twist twist)
 
 }
 
-void writeToPort(SerialPort *ser, char* write)
+void writeToPort(serial::Serial *ser, char* write)
 {
 	bool written=false;
-	while(!written){
+	int count = 0;
+	while(!written && count<10){
+		count++;
 		try{
-			ser->Write(write);
+			ser->write(write);
 			written=true;
+			std::cout<<"wrote\n";
 		}
 		catch(std::exception& e){
+			std::cout<<"exception\n"<<count;
+			std::cerr<<e.what();
 			continue;
 		}
+		
+	}
+	if (written==false)
+	{
+		ser->close();
+		ser->open();
+		std::cout<<"Could not write\n";
 	}
 }
 
-void writeDeltas(SerialPort *ser1, SerialPort *ser2, float *rates){
+void writeDeltas(serial::Serial *ser1, serial::Serial *ser2, float *rates){
 		
 		//TODO: make this more straightforward
 		char output_buffer[64];
@@ -98,79 +153,113 @@ void writeDeltas(SerialPort *ser1, SerialPort *ser2, float *rates){
 		char* write =(char*) malloc(len+1);
 		strncpy(write,output_buffer,len);
 		write[len]='\0';
+		std::cout<<"write to port 1\n";
 		writeToPort(ser1,write);
-		//std::cout<<"wrote "<<write<<"\n";
+		std::cout<<"wrote 1";//<<write<<"\n";
 
 		len=sprintf(output_buffer,"!g 2 %d\r",(int)rates[1]);
 		write =(char*) malloc(len+1);
 		strncpy(write,output_buffer,len);
 		write[len]='\0';
+		std::cout<<"write to port 2\n";
 		writeToPort(ser1,write);
 		//std::cout<<"wrote "<<write<<"\n";
-
+		std::cout<<"wrote 2";
+		
 		len=sprintf(output_buffer,"!g 1 %d\r",(int)rates[2]);
 		write =(char*) malloc(len+1);
 		strncpy(write,output_buffer,len);
 		write[len]='\0';
+		std::cout<<"write to port 3\n";
 		writeToPort(ser2,write);
 		//std::cout<<"wrote "<<write<<"\n";
+		std::cout<<"wrote 3";
 
 		len=sprintf(output_buffer,"!g 2 %d\r",(int)rates[3]);
 		write =(char*) malloc(len+1);
 		strncpy(write,output_buffer,len);
 		write[len]='\0';
+		std::cout<<"write to port 4\n";
 		writeToPort(ser2,write);
 		//std::cout<<"wrote "<<write<<"\n";
+		std::cout<<"wrote 4"<<std::endl;
 }
 
-int main(int argc, char** argv){
 
+int main(int argc, char** argv){
+	//signal(SIGINT,int_handler);
 	ros::init(argc,argv,"orbot_client");
 	ros::NodeHandle nh;
 	//viconSub=nh.subscribe("/vicon/Orbot/Orbot",1000,messageCallbackVicon);
-	velcmd = nh.subscribe("/orbot/cmd_vel",1000,velocityCallback);
-	viconspeed = nh.subscribe("/orbot_rover/local_odom",1000,viconCallback);
-	ros::Rate loop_rate(30);//TODO: spiit up the updating and writing rates with time class
+	velcmd = nh.subscribe("/orbot/cmd_vel",10,velocityCallback);
+	viconspeed = nh.subscribe("/orbot_rover/local_odom",10,viconCallback);
+	armcmd = nh.subscribe("/orbot/arm_pose",10,armCallback);
+	ros::Rate loop_rate(10);//TODO: split up the updating and writing rates with time class
 	
 	
 	ros::Time prevWrite=ros::Time::now();
 	ros::Time start_time=ros::Time::now();
-	ros::Duration writeDelay(.01);
+	ros::Duration writeDelay(.1);
 	ros::Duration goalDelay(0.5);
 	ros::Duration d;
 	
 	double t;
+	serial::Serial *ser1;//("/dev/ttyACM0");
+	serial::Serial *ser2;//("/dev/ttyACM1");
+	ser1 = new serial::Serial();
+	ser2 = new serial::Serial();
+	ser1->setPort("/dev/ttyACM0");
+	ser1->open();
+	ser1->setBaudrate(115200);
 	
-	SerialPort ser1("/dev/ttyACM0");
-	ser1.Open();
-	ser1.SetBaudRate(SerialPort::BAUD_115200);
+	ser2->setPort("/dev/ttyACM1");
+	ser2->open();
+	ser2->setBaudrate(115200);
 	
-	SerialPort ser2("/dev/ttyACM1");
-	ser2.Open();
-	ser2.SetBaudRate(SerialPort::BAUD_115200);
-		
-
+	actionlib::SimpleActionClient<kinova_msgs::ArmPoseAction> ac("/m1n6s300_driver/pose_action/tool_pose", true);
+	ROS_INFO("Waiting for action server to start.");
+	ac.waitForServer(); //will wait for infinite time
+	ROS_INFO("Action server started, sending goal.");
+	//ros::AsyncSpinner spinner(5);
 	while(ros::ok()){
 		
+		//spinner.start();
 		bool write=false;
 		//std::cout<<"Still Running\n";
 		if(ros::Time::now()-prevGoal>goalDelay)
 			GoalTimeout = true;
 
-		if(ros::Time::now()-prevWrite>writeDelay && !GoalTimeout){
+		if(ros::Time::now()-prevWrite>writeDelay){ //&& !GoalTimeout){
 			
 			write=false;
-			writeDeltas(&ser1,&ser2,rates); //Rover Uncomment
+			std::cout<<"Going to write deltas\n";
+			writeDeltas(ser1,ser2,rates); //Rover Uncomment
 			prevWrite=ros::Time::now();
 			std::cout<<"Wrote speeds: "<<rates[0]<<" "<<rates[1]<<" "<<rates[2]<<" "<<rates[3]<<"\n";
 		}
 		
-//		loop_rate.sleep();
+		if(ros::Time::now()-armTime>armDelay && update)
+		{
+			update = false;
+			ac.cancelGoal();
+			ac.sendGoal(goal);
+			armTime = ros::Time::now();
+			bool finished_before_timeout = ac.waitForResult(ros::Duration(3.0));		
+			if (finished_before_timeout)
+		  		{
+		    	actionlib::SimpleClientGoalState state = ac.getState();
+		    	ROS_INFO("Action finished: %s",state.toString().c_str());
+		  		}
+		  	else
+		    	ROS_INFO("Action did not finish before the time out.");
+		}
+
 		ros::spinOnce();
 		loop_rate.sleep();
 	}
-	ser1.Close();// Rover Uncomment
-	ser2.Close();// Rover Uncomment
+	//spinner.stop();
+	ser1->close();// Rover Uncomment
+	ser2->close();// Rover Uncomment
 	return 0;
 }
 
