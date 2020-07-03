2b0e112654f447cddc8e0673173a261f28381c04
root <sparjunram94@gmail.com>
2020-07-03 16:54:24 -0500

    Working code from odroid

--- src/rover_control.cpp
+++ src/rover_control.cpp
@@ -101,7 +101,9 @@ void velocityCallback(geometry_msgs::Twist twist)
 	goal_vel[0] = twist.linear.x;
 	goal_vel[1] = twist.linear.y;
 	goal_vel[2] = twist.angular.z;
+	std::cout<<"Speed:"<<twist.linear.x<<" "<<twist.linear.y<<" "<<twist.angular.z<<"\n";
 	getRotationRates(rates,goal_vel,real_vel);
+	//std::cout<<"Calculated speeds: "<<rates[0]<<" "<<rates[1]<<" "<<rates[2]<<" "<<rates[3]<<"\n";
 	for(int i=0;i<4;i++){
 	rates[i]=rates[i]/pi/2*60;
 		if(rates[i]>120)
@@ -128,7 +130,7 @@ void writeToPort(serial::Serial *ser, char* write)
 		try{
 			ser->write(write);
 			written=true;
-			std::cout<<"wrote\n";
+			//std::cout<<"wrote\n";
 		}
 		catch(std::exception& e){
 			std::cout<<"exception\n"<<count;
@@ -147,42 +149,52 @@ void writeToPort(serial::Serial *ser, char* write)
 
 void writeDeltas(serial::Serial *ser1, serial::Serial *ser2, float *rates){
 		
+		ser1->flush();
+		ser2->flush();
 		//TODO: make this more straightforward
-		char output_buffer[64];
+		char output_buffer[10];
 		int len=sprintf(output_buffer,"!g 1 %d\r",(int)rates[0]);
 		char* write =(char*) malloc(len+1);
 		strncpy(write,output_buffer,len);
 		write[len]='\0';
-		std::cout<<"write to port 1\n";
+		std::cout<<output_buffer<<std::endl;
+		//std::cout<<"write to port 1 "<<(int)rates[0]<<std::endl;
 		writeToPort(ser1,write);
-		std::cout<<"wrote 1";//<<write<<"\n";
-
+		//std::cout<<"wrote 1";//<<write<<"\n";
+		ser1->flush();
 		len=sprintf(output_buffer,"!g 2 %d\r",(int)rates[1]);
 		write =(char*) malloc(len+1);
 		strncpy(write,output_buffer,len);
 		write[len]='\0';
-		std::cout<<"write to port 2\n";
+		std::cout<<output_buffer<<std::endl;
+		//std::cout<<"write to port 2 "<<(int)rates[1]<<std::endl;
+		//std::cout<<"write to port 2\n";
 		writeToPort(ser1,write);
 		//std::cout<<"wrote "<<write<<"\n";
-		std::cout<<"wrote 2";
-		
+		//std::cout<<"wrote 2";
+		ser2->flush();
 		len=sprintf(output_buffer,"!g 1 %d\r",(int)rates[2]);
 		write =(char*) malloc(len+1);
 		strncpy(write,output_buffer,len);
 		write[len]='\0';
-		std::cout<<"write to port 3\n";
+		std::cout<<output_buffer<<std::endl;
+		//std::cout<<"write to port 3 "<<(int)rates[2]<<std::endl;
+		//std::cout<<"write to port 3\n";
 		writeToPort(ser2,write);
 		//std::cout<<"wrote "<<write<<"\n";
-		std::cout<<"wrote 3";
-
+		//std::cout<<"wrote 3";
+		ser2->flush();
 		len=sprintf(output_buffer,"!g 2 %d\r",(int)rates[3]);
 		write =(char*) malloc(len+1);
 		strncpy(write,output_buffer,len);
 		write[len]='\0';
-		std::cout<<"write to port 4\n";
+		std::cout<<output_buffer<<std::endl;
+		//std::cout<<"write to port 4 "<<(int)rates[3]<<std::endl;
+		//std::cout<<"write to port 4\n";
 		writeToPort(ser2,write);
 		//std::cout<<"wrote "<<write<<"\n";
-		std::cout<<"wrote 4"<<std::endl;
+		//std::cout<<"wrote 4"<<std::endl;
+		delete write;
 }
 
 
@@ -191,7 +203,7 @@ int main(int argc, char** argv){
 	ros::init(argc,argv,"orbot_client");
 	ros::NodeHandle nh;
 	//viconSub=nh.subscribe("/vicon/Orbot/Orbot",1000,messageCallbackVicon);
-	velcmd = nh.subscribe("/orbot/cmd_vel",10,velocityCallback);
+	velcmd = nh.subscribe("/orbot/cmd_vel",1,velocityCallback);
 	viconspeed = nh.subscribe("/orbot_rover/local_odom",10,viconCallback);
 	armcmd = nh.subscribe("/orbot/arm_pose",10,armCallback);
 	ros::Rate loop_rate(10);//TODO: split up the updating and writing rates with time class
@@ -200,7 +212,7 @@ int main(int argc, char** argv){
 	ros::Time prevWrite=ros::Time::now();
 	ros::Time start_time=ros::Time::now();
 	ros::Duration writeDelay(.1);
-	ros::Duration goalDelay(0.5);
+	ros::Duration goalDelay(2);
 	ros::Duration d;
 	
 	double t;
@@ -215,12 +227,14 @@ int main(int argc, char** argv){
 	ser2->setPort("/dev/ttyACM1");
 	ser2->open();
 	ser2->setBaudrate(115200);
-	
+	ser1->flush();
+	ser2->flush();
 	actionlib::SimpleActionClient<kinova_msgs::ArmPoseAction> ac("/m1n6s300_driver/pose_action/tool_pose", true);
 	ROS_INFO("Waiting for action server to start.");
-	ac.waitForServer(); //will wait for infinite time
+	//ac.waitForServer(); //will wait for infinite time
 	ROS_INFO("Action server started, sending goal.");
-	//ros::AsyncSpinner spinner(5);
+	ros::AsyncSpinner spinner(5);
+	int writecounter = 0;
 	while(ros::ok()){
 		
 		//spinner.start();
@@ -229,16 +243,25 @@ int main(int argc, char** argv){
 		if(ros::Time::now()-prevGoal>goalDelay)
 			GoalTimeout = true;
 
-		if(ros::Time::now()-prevWrite>writeDelay){ //&& !GoalTimeout){
+		if(ros::Time::now()-prevWrite>writeDelay){// && !GoalTimeout){
 			
 			write=false;
-			std::cout<<"Going to write deltas\n";
+			//std::cout<<"Going to write deltas\n";
+			
 			writeDeltas(ser1,ser2,rates); //Rover Uncomment
 			prevWrite=ros::Time::now();
+			writecounter++;
+			if(writecounter>100)
+			{
+				writecounter = 0;
+				ser1->flush();
+				ser2->flush();
+				std::cout<<"Flushed\n";
+			}
 			std::cout<<"Wrote speeds: "<<rates[0]<<" "<<rates[1]<<" "<<rates[2]<<" "<<rates[3]<<"\n";
 		}
 		
-		if(ros::Time::now()-armTime>armDelay && update)
+		/*if(ros::Time::now()-armTime>armDelay && update)
 		{
 			update = false;
 			ac.cancelGoal();
@@ -252,7 +275,7 @@ int main(int argc, char** argv){
 		  		}
 		  	else
 		    	ROS_INFO("Action did not finish before the time out.");
-		}
+		}*/
 
 		ros::spinOnce();
 		loop_rate.sleep();
@@ -279,9 +302,12 @@ void getRotationRates(float* rates, float* goal_vel,float* real_vel, float x_cm,
 		//float b=(((i&2)>>1)^(i&1))>0?-pi/2:pi/2;
 		float c=i&1>0?-pi/4:pi/4;
 		float l=sqrt(wheel_locs[0][i]*wheel_locs[0][i]+wheel_locs[1][i]*wheel_locs[1][i]);
-		rates[i]=(goal_vel[0] + 0*(goal_vel[0] - real_vel[0]))/radius*cos(b[i]-c)/sin(c);
-		rates[i]-=(goal_vel[1] + 0*(goal_vel[1] - real_vel[1]))/radius*sin(b[i]-c)/sin(c);
-		rates[i]-=(goal_vel[2] + 0*(goal_vel[2] - real_vel[2]))/radius*l*sin(b[i]-c+a)/sin(c);
+		//rates[i]=(goal_vel[0] + 0*(goal_vel[0] - real_vel[0]))/radius*cos(b[i]-c)/sin(c);
+		//rates[i]-=(goal_vel[1] + 0*(goal_vel[1] - real_vel[1]))/radius*sin(b[i]-c)/sin(c);
+		//rates[i]-=(goal_vel[2] + 0*(goal_vel[2] - real_vel[2]))/radius*l*sin(b[i]-c+a)/sin(c);
+		rates[i]=(goal_vel[0])/radius*cos(b[i]-c)/sin(c);
+		rates[i]-=(goal_vel[1])/radius*sin(b[i]-c)/sin(c);
+		rates[i]-=(goal_vel[2])/radius*l*sin(b[i]-c+a)/sin(c);
 	}
 
 }
